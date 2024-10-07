ca6815450b20f7bc22c09dd2e7f569a74eab65ec
Ahmed Fetyan <midomigo1@aucegypt.edu>
2024-10-07 15:44:51 +0300

    create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,44 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/Views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-          color: Color(0xffFFCC80), borderRadius: BorderRadius.circular(16)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+            color: Color(0xffFFCC80), borderRadius: BorderRadius.circular(16)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16.0, bottom: 16),
+                child: Text('Build your career with Maged',
+                    style: TextStyle(
+                        color: Colors.black.withOpacity(0.5), fontSize: 18)),
+              ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 24,
+                  )),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16.0, bottom: 16),
-              child: Text('Build your career with Maged',
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text('October6, 2024',
                   style: TextStyle(
-                      color: Colors.black.withOpacity(0.5), fontSize: 18)),
-            ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                  size: 24,
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text('October6, 2024',
-                style: TextStyle(
-                    color: Colors.black.withOpacity(0.4), fontSize: 16)),
-          )
-        ],
+                      color: Colors.black.withOpacity(0.4), fontSize: 16)),
+            )
+          ],
+        ),
       ),
     );
   }
